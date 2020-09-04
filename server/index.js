780b2182e6ad6a6faa56d1d8bf73d2582ff5b926
faith <fmuchembi@gmail.com>
2020-09-04 16:32:39 +0300

    :zap: update

--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const pool = require("./db");
+const routes = require('router')
 
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
+
+
 //routes
-//app.use("/data", require("./routes/nairobiHealth"));
+app.use("/data", require("./routes/nairobiHealth"));
 
 
 app.listen(9000, () => {
