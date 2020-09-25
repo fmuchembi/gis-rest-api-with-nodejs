74292ee6894c449e70f9a157ed917f4ef687d2e8
PondiB <pondibrian@gmail.com>
2020-09-25 14:35:05 +0300

    :sunglasses: New features -Get nearest health facilities and within a sub county

--- server/routes/nairobiHealth.js
+++ server/routes/nairobiHealth.js
@@ -28,6 +28,35 @@ router.get("/nairobisubcounties" , async(req, res) =>{
 });
 
 
+////get Nearest Health Facilities
+router.get("/nairobihealthfacilities/userlocation=x,y", async(req, res)=>{
+    try{
+        const allNairobiHealthFacilities = await pool.query("SELECT nhf.id, nhf.name, nhf.geom, ST_Distance(nhf.geom,ST_SetSRID(ST_Point(:userLongitude,:userLatitude),4326)) AS distance "
+                                                             + "FROM nairobi_Health_facilities nhf "
+                                                             + "ORDER BY nhf.geom  <-> ST_SetSRID(ST_Point(:userLongitude,:userLatitude),4326) "
+                                                             + "LIMIT 5",{userLongitude:req.userlocation[0],userLatitude:req.userlocation[1]});
+        res.json(allNairobiHealthFacilities.rows);
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+});
+
+////get Health Facilities withib a SubCounty
+router.get("/nairobihealthfacilities/subcountyId}", async(req, res)=>{
+    try{
+        const nairobiHealthFacilitiesWithinSubCounty = await pool.query("SELECT nhf.id, nhf.name, nhf.geom "
+                                                                        +"FROM nairobi_Health_facilities nhf, nairobi_sub_counties nsc" 
+                                                                        +"WHERE ST_Within(nhf.geom, nsc.geom) AND nsc.id=(:id)", {id: req.subcountyId});
+        res.json(nairobiHealthFacilitiesWithinSubCounty.rows);
+
+    }catch(err){ 
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+});
+
 
 
 module.exports= router;
