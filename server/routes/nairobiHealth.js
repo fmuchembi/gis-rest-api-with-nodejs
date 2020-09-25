5c417416833babdc9d1264c4d58e6a1527dfd684
faith <fmuchembi@gmail.com>
2020-09-25 13:45:17 +0300

    update

--- server/routes/nairobiHealth.js
+++ server/routes/nairobiHealth.js
@@ -5,12 +5,25 @@ const pool = require("../db");
 //get all nairobi health facilities
 router.get("/nairobihealthfacilities", async(req, res)=>{
     try{
-        const allNairobiHealthFacilities = await pool.query("SELECT id, st_astext(geom) as geometry, name FROM public.nairobi_health_facilities");
+        const allNairobiHealthFacilities = await pool.query("SELECT id, ST_AsGeojson(geom)::json as point, name FROM public.nairobi_health_facilities");
         res.json(allNairobiHealthFacilities.rows);
 
     }catch(err){
         console.error(err.message);
         res.status(500).send("server error");
+    }
+});
+
+////get Nairobi Sub-counties
+router.get("/nairobisubcounties" , async(req, res) =>{
+    try{
+        const nairobiSubcounties = await pool.query("SELECT id, ST_AsGeojson(geom)::json as point, name FROM public.nairobi_sub_counties");
+        res.json(nairobiSubcounties.rows);
+
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('server error' + err.message);
     }
 });
 
