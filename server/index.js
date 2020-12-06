74b57438dd3967f747bd29f1e15e787ac619f8ef
faith <fmuchembi@gmail.com>
2020-12-06 21:17:40 +0300

    remove unused function

--- server/index.js
+++ server/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const pool = require("./db");
 
 
 //middleware
 app.use(express.json());
 app.use(cors());
-
-
-
-//routes
-app.use("/data", require("./routes/nairobiHealth"));
 
 
 app.listen(9000, () => {
