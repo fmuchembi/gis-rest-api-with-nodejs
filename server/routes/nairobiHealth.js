0cae5c4880673b9f6fd467d725bd3aba1af59865
faith <fmuchembi@gmail.com>
2020-12-11 23:32:48 +0300

    bug fix

--- server/routes/nairobiHealth.js
+++ server/routes/nairobiHealth.js
@@ -29,9 +29,8 @@ router.get("/api/nairobisubcounties" , async(req, res) =>{
 
 
 ////get Nearest Health Facilities
-router.get("/api/nairobihealthfacilities/nearerstfacility?userlocation=x,y", async(req, res)=>{
+router.get("/api/nairobihealthfacilities/nearerstfacility/:userlocation=x,y", async(req, res)=>{
     try{
-        //const {userlocation} = req.params;
         const allNairobiHealthFacilities = await pool.query("SELECT nhf.id, nhf.name, ST_AsGeojson(nhf.geom)::json, ST_Distance(nhf.geom,ST_SetSRID(ST_Point(:userLongitude,:userLatitude),4326)) AS distance "
                                                              + "FROM nairobi_Health_facilities nhf "
                                                              + "ORDER BY nhf.geom  <-> ST_SetSRID(ST_Point(:userLongitude,:userLatitude),4326) "
@@ -40,22 +39,22 @@ router.get("/api/nairobihealthfacilities/nearerstfacility?userlocation=x,y", asy
 
     }catch(err){
         console.error(err.message);
-        res.status(500).send("server error");
+        res.status(500).send("server error" + err.message);
     }
 });
 
 ////get Health Facilities within a SubCounty
 router.get("/api/nairobihealthfacilities/withinsubcounty/:name", async(req, res)=>{
     try{
-        const {name} = req.body;
+        const {name} = req.params;
         const nairobiHealthFacilitiesWithinSubCounty = await pool.query("SELECT nhf.id, nhf.name, nhf.geom "
                                                                         +"FROM nairobi_Health_facilities nhf, nairobi_sub_counties nsc" 
-                                                                        +"WHERE ST_Within(nhf.geom, nsc.geom) AND nsc.name=(:name)", [name]);
+                                                                        +"WHERE ST_Within(nhf.geom, nsc.geom) AND nsc.name=$1", [name]);
         res.json(nairobiHealthFacilitiesWithinSubCounty.rows);
 
     }catch(err){ 
         console.error(err.message);
-        res.status(500).send("server error");
+        res.status(500).send("server errors" + err.message);
     }
 });
 
