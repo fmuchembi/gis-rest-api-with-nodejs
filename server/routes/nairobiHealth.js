46ca93d08cc5742b06989547aecfa382220137fb
faith <fmuchembi@gmail.com>
2021-04-20 11:59:17 +0300

    update

--- server/routes/nairobiHealth.js
+++ server/routes/nairobiHealth.js
@@ -29,34 +29,9 @@ router.get("/api/nairobisubcounties" , async(req, res) =>{
 
 
 ////get Nearest Health Facilities
-router.get("/api/nairobihealthfacilities/nearerstfacility/:userlocation=x,y", async(req, res)=>{
-    try{
-        const allNairobiHealthFacilities = await pool.query("SELECT nhf.id, nhf.name, ST_AsGeojson(nhf.geom)::json, ST_Distance(nhf.geom,ST_SetSRID(ST_Point(:userLongitude,:userLatitude),4326)) AS distance "
-                                                             + "FROM nairobi_Health_facilities nhf "
-                                                             + "ORDER BY nhf.geom  <-> ST_SetSRID(ST_Point(:userLongitude,:userLatitude),4326) "
-                                                             + "LIMIT 5",{userLongitude:userlocation[0],userLatitude:userlocation[1]});
-        res.json(allNairobiHealthFacilities.rows);
-
-    }catch(err){
-        console.error(err.message);
-        res.status(500).send("server error" + err.message);
-    }
-});
 
 ////get Health Facilities within a SubCounty
-router.get("/api/nairobihealthfacilities/withinsubcounty/:name", async(req, res)=>{
-    try{
-        const {name} = req.params;
-        const nairobiHealthFacilitiesWithinSubCounty = await pool.query("SELECT nhf.id, nhf.name, nhf.geom "
-                                                                        +"FROM nairobi_Health_facilities nhf, nairobi_sub_counties nsc" 
-                                                                        +"WHERE ST_Within(nhf.geom, nsc.geom) AND nsc.name=$1", [name]);
-        res.json(nairobiHealthFacilitiesWithinSubCounty.rows);
 
-    }catch(err){ 
-        console.error(err.message);
-        res.status(500).send("server errors" + err.message);
-    }
-});
 
 
 
